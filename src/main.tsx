66fdef03037f3b62d54960fb50ed631ce93b24ec
strikeitbroke <strikeitbroke@gmail.com>
2026-02-28 20:19:52 -0800

    use react ga4

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import ReactGA from "react-ga4";
 import "flowbite";
 import "./index.css";
 import App from "./App.tsx";
+
+if (import.meta.env.PROD) {
+  ReactGA.initialize("G-EZYNVSP8VB");
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
